6ba5204f09c3a58d0cf64bb48057074030228bf9
Hector Gonzalez <hectarek13@gmail.com>
2020-10-06 15:32:41 -0400

    close

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { readFile, readFileSync } = require('fs');
+const myModule = require('./my-module');
+const express = require('express');
+// const { readFile, readFileSync } = require('fs');
+const { readFile } = require('fs').promises;
 
 // // Using readFileSync will read the file in a synchronous way. 
 // // In this example, the second console log won't run until the first one is done.
@@ -11,14 +14,41 @@ const { readFile, readFileSync } = require('fs');
 
 // // OR
 
-// doing it this way will make it so that everything runs
-// according to its order in the event loop. Because 
-// the readFile has a call back function, the second log
-// will execute first.
+// // doing it this way will make it so that everything runs
+// // according to its order in the event loop. Because 
+// // the readFile has a call back function, the second log
+// // will execute first.
 
-readFile('./hello.txt', 'utf8', (err, txt) => {
-    console.log(txt);
-})
+// readFile('./hello.txt', 'utf8', (err, txt) => {
+//     console.log(txt);
+// })
 
-console.log('do this ASAP');
+// console.log('do this ASAP');
+
+
+// THERE IS A THIRD WAY THAT INCLUDES PROMISES
+
+// this will include using require.('fs').promises;
+
+// // THIS WILL WORK, however is very new and not as recognized yet.
+// const file = await readFile('./hello.txt', 'utf8');
+
+async function hello() {
+    const file = await readFile('./hello.txt', 'utf8');
+}
+
+hello();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    readFile('./index.html', 'utf8', (err, html) => {
+        if (err) {
+            res.status(500).send("Sorry, it don't work")
+        }
+        res.send(html);
+    })
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`));
 
